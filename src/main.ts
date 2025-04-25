4d201d6622413f5c1bcf434a0390d06e73f0a7b6
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-04-25 16:41:59 -0300

    Agregando CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*', // 🔒 Podés reemplazar esto por ['http://localhost:3000'] si querés restringirlo
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
